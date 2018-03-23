89f7b3096b29609269c1a008a2c42ecdacc7f7ff
Bartosz Oczujda <bartosz.oczujda@gmail.com>
2018-03-23 07:14:41 +0100

    Initial commit

---
+++ create.js
@@ -0,0 +1,53 @@
+import uuid from 'uuid'
+import AWS from 'aws-sdk'
+
+AWS.config.update({region: 'eu-west-1'})
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient()
+
+const main = (event, context, callback) => {
+
+    const data = JSON.parse(event.body)
+
+    const params = {
+
+        TableName: 'notes',
+        Item: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            noteId: uuid.v1(),
+            content: data.content,
+            attachment: data.attachment,
+            createdAt: new Date().getTime()
+        }
+
+    }
+
+    dynamoDB.put(params, (error, data) => {
+
+        const headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+        }
+
+        if (error) {
+
+            const response = {
+                statusCode: 500,
+                headers: headers,
+                body: JSON.stringify({ status: false })
+            }
+
+            callback(null, response)
+            return
+        }
+      
+        const response = {
+            statusCode: 200,
+            headers: headers,
+            body: JSON.stringify(params.Item)
+        }
+
+        callback(null, response)
+    })
+
+}
